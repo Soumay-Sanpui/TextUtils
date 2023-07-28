53529f10ffd66689ee7b4a2c899c1f0f2e4708f9
Somu sanpui <somusanpui9@gmail.com>
2023-07-28 13:38:24 +0530

    added deployment file

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-
-    /** If you set esmExternals to true, this plugins assumes that 
-      all external dependencies are ES modules */
-
-    commonjsOptions: {
-      esmExternals: true
-    },
-  }
+  base: "/vite-deploy/"
 })
